d93cf484edf19c6fc1738b4a45f7405cd1998331
William Rios <133013480+Winnd11@users.noreply.github.com>
2024-08-23 14:47:33 -0300

    Update App.java

--- random/src/App.java
+++ random/src/App.java
@@ -14,17 +14,5 @@ public class App {
 
         char c = (char) ('a' + r.nextInt(26));
         System.out.println(c);
-
-        // parse string to int
-
-        int number10 = 10;
-        String number10String = Integer.toString(number10);
-        System.out.println(number10String);
-
-        // OR
-
-        int number20 = 20;
-        String number20String = String.valueOf(number20);
-        System.out.println(number20String);
     }
 }
