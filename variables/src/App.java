c9fcd0d49460a755191988931c34003200e00e6f
Winnd11 <recuperacaowilliamrios@gmail.com>
2024-08-23 16:30:38 -0300

    Add variables.js

---
+++ variables/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        final int NUMBER = 10;
+
+        System.out.println(NUMBER);
+
+        boolean day = true;
+
+        day = !day;
+
+        System.out.println(day);
+    }
+}
