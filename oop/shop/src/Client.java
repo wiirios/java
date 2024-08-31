92f2ebfeffb5f96c66a53f5206b54c31d8ce5237
William Rios <133013480+Winnd11@users.noreply.github.com>
2024-08-30 21:21:19 -0300

    Update Client.java

--- oop/shop/src/Client.java
+++ oop/shop/src/Client.java
@@ -1,4 +1,13 @@
 public class Client {
     String name;
     int balance;
+
+    public Client(String name, int balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public void withdrawBalance(int price) {
+        balance -=  price;
+    }
 }
