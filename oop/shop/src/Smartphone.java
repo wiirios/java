27d2d2363778ebc29d63439229195b45c9495f8a
William Rios <133013480+Winnd11@users.noreply.github.com>
2024-08-30 21:21:45 -0300

    Update Smartphone.java

--- oop/shop/src/Smartphone.java
+++ oop/shop/src/Smartphone.java
@@ -4,4 +4,15 @@ public class Smartphone {
     String os;
     int memory;
     int memoryRam;
+    int price;
+
+    public Smartphone (String comercialName, String brand, String model, String os, int memory, int memoryRam, int price) {
+        this.comercialName = comercialName;
+        this.brand = brand;
+        this.model = model;
+        this.os = os;
+        this.memory = memory;
+        this.memoryRam = memoryRam;
+        this.price = price;
+    }
 }
