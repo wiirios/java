6c526636f912dcf5f10456c8f1b003324d290b9e
William Rios <133013480+Winnd11@users.noreply.github.com>
2024-09-09 12:41:27 -0300

    Update PhoneBookRepository.java

--- phone-book/src/org/william/repository/PhoneBookRepository.java
+++ phone-book/src/org/william/repository/PhoneBookRepository.java
@@ -1,33 +1,33 @@
-package org.william.repository;
+// package org.william.repository;
 
-import org.william.service.PhoneBookService;
+// import org.william.service.PhoneBookService;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.io.FileWriter;
+// import java.io.FileNotFoundException;
+// import java.io.IOException;
+// import java.nio.file.FileAlreadyExistsException;
+// import java.nio.file.Files;
+// import java.nio.file.Path;
+// import java.nio.file.Paths;
+// import java.io.FileWriter;
 
-public class PhoneBookRepository{
-    Path path = Paths.get("src/org/william/contacts", "contacts.txt");
-    PhoneBookService phoneBookService = new PhoneBookService();
+// public class PhoneBookRepository{
+//     Path path = Paths.get("src/org/william/contacts", "contacts.txt");
+//     PhoneBookService phoneBookService = new PhoneBookService();
 
-    public void createFile() throws IOException {
-        try {
-            Files.createFile(path);
-        } catch (FileAlreadyExistsException e) {
-            System.out.println("File already exist");
-        }
-    }
+//     public void createFile() throws IOException {
+//         try {
+//             Files.createFile(path);
+//         } catch (FileAlreadyExistsException e) {
+//             System.out.println("File already exist");
+//         }
+//     }
 
-    public void writeFile() throws IOException {
-        try (FileWriter fileWriter = new FileWriter(String.valueOf(path))) {
-            //fileWriter.write(String.valueOf(phoneBookService.getPerson()));
-            System.out.println("Success");
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
-        }
-    }
-}
+//     public void writeFile() throws IOException {
+//         try (FileWriter fileWriter = new FileWriter(String.valueOf(path))) {
+//             fileWriter.write(String.valueOf(phoneBookService.getPerson()));
+//             System.out.println("Success");
+//         } catch (FileNotFoundException e) {
+//             System.out.println("File not found");
+//         }
+//     }
+// }
