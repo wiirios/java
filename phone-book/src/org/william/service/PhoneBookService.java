d76e4330a786797d4a58881c57bf6580a4eaa038
William Rios <133013480+Winnd11@users.noreply.github.com>
2024-09-09 12:41:10 -0300

    Update PhoneBookService.java

--- phone-book/src/org/william/service/PhoneBookService.java
+++ phone-book/src/org/william/service/PhoneBookService.java
@@ -1,20 +1,33 @@
 package org.william.service;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Map;
 
 public class PhoneBookService {
     HashMap<String, String> person = new HashMap<String, String>();
+    Path path = Paths.get("src/org/william/contacts", "contacts.txt");
 
-    public void addPerson(String addName, String addCellphone) {
-        person.put(addName, addCellphone);
-    }
+    // public void addPerson(String addName, String addCellphone) {
+    //     person.put(addName, addCellphone);
+    // }
 
-    public String getPerson() {
-                person.forEach((k, v) -> {
-            System.out.println(k + " " + v);
-        });
-    }
+    // public String getPerson() {
+    //             person.forEach((k, v) -> {
+    //         System.out.println(k + " " + v);
+    //     });
+    // }
 
-    public HashMap<String, String> returnPerson() {
-        return person;
+    public void writeTxt() throws IOException {
+        try (FileWriter fileWriter = new FileWriter(String.valueOf(path))) {
+            for (Map.Entry<String, String> entry : person.entrySet()) {
+                fileWriter.write(entry.getKey() + " " + entry.getValue() +"\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
