64f0bfa6c39b0684d819c366bbec2c479da72e5c
William Rios <133013480+Winnd11@users.noreply.github.com>
2024-09-10 10:41:03 -0300

    Add files via upload

---
+++ abstraction/src/Animal.java
@@ -0,0 +1,5 @@
+abstract class Animal {
+    public void eat() {
+        System.out.println("te");
+    }
+}
---
+++ abstraction/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Ant ant = new Ant();
+
+        ant.eat();
+    }
+}
