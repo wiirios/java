d605c50583675d5012f2235df58c9b6617af952c
William Rios <133013480+Winnd11@users.noreply.github.com>
2024-09-10 09:37:34 -0300

    Add files via upload

---
+++ inheritance/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Car alpine = new Car();
+        Airplane boeing = new Airplane();
+
+        alpine.turnOn();
+        boeing.turnOn();
+    }
+}
---
+++ inheritance/src/Vehicle.java
@@ -0,0 +1,5 @@
+public class Vehicle {
+    public void turnOn() {
+        System.out.println("turn on");
+    }
+}
