e78cd5e8ed684de1cc84d1a9f96e1fabfa6bf792
William Rios <133013480+Winnd11@users.noreply.github.com>
2024-11-12 22:16:25 -0300

    Add files via upload

---
+++ data-structure/bubblesort/src/org/william/Main.java
@@ -0,0 +1,31 @@
+package org.william;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] vector = new int[7];
+		int vectorLength = vector.length;
+		
+		for (int i = 0; i < vectorLength; i++) {
+			vector[i] = (int) (Math.random() * 10);
+			System.out.println(vector[i]);
+ 		}
+		
+		for (int i = 0; i < vectorLength; i++) {
+			for (int j = i + 1; j < vectorLength; j++) {
+				if (vector[i] > vector[j]) {
+					int a = vector[i];
+					vector[i] = vector[j];
+					vector[j] = a;
+					
+				}
+			}
+		}
+		
+		System.out.println("-------------");
+		
+		for (int i = 0; i < vectorLength; i++) {
+			System.out.println(vector[i]);
+ 		}
+		
+	}
+}
