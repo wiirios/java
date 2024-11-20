83789558e4a4ab06516b03f05c488a3fe3e94afb
William Rios <133013480+wiirios@users.noreply.github.com>
2024-11-20 11:07:30 -0300

    Update Main.java

--- streamApi/src/streamApi/org/william/Main.java
+++ streamApi/src/streamApi/org/william/Main.java
@@ -14,9 +14,9 @@ public class Main {
 		list.add(new Client("John", 21));
 		
 		list.stream().forEach(c -> System.out.println(c.getName()));
-		
+
+		System.out.println("Person Filter");
 		System.out.println("----");		
-		
 		List<Client> clientsFilter = list.stream()
 			.filter(c -> c.getAge() > 30)
 			.collect(Collectors.toList());
@@ -24,7 +24,37 @@ public class Main {
 		clientsFilter.stream()
 			.forEach(c -> System.out.println(c.getName()));
 		
-			
+		List<Integer> listNumbers = new ArrayList<Integer>();		
+
+		listNumbers.add(1);
+		listNumbers.add(9);
+		listNumbers.add(4);
+		listNumbers.add(8);
+		listNumbers.add(8);
+		System.out.println("----");		
+		System.out.println("Print each number in listNumbers (not sorted)");		
+		System.out.println("----");	
+		listNumbers.stream()
+				.forEach(System.out::println);
+
+	 	List<Integer> numbersSorted = listNumbers
+	 			.stream()
+	 			.sorted()
+	 			.collect(Collectors.toList());
+		
+		System.out.println("----");		
+		System.out.println("Numbers Sorted");		
+		System.out.println("----");
+		numbersSorted.stream()
+				.forEach(System.out::println);	
+		
+		System.out.println("----");		
+		System.out.println("Print the numbers that are even and remove duplicate numbers");		
+		System.out.println("----");	
+		listNumbers.stream()
+				.filter(n -> n % 2 == 0)
+				.distinct()
+				.forEach(System.out::println);
 	}
 
 }
